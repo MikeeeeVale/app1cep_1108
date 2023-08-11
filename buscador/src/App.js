3ab67a43085b2da7dcbc5cd75d70bc418b462f1f
MikeeeeVale <mikevale.etec.sp.gov.br@gmail.com>
2023-08-11 13:37:09 +0000

    React1108

---
+++ buscador/src/App.js
@@ -0,0 +1,17 @@
+import { LuHeart } from "react-icons/lu";
+
+function App() {
+  return (
+    <div className="container">
+     <h1 className="title"> Buscador de CEP</h1>
+     <div className="containerInput">
+      <input type="text" placeholder="Digite aqui..."/>
+      <button className="buttonSearch">
+        <LuHeart size={25} color="red"/>
+      </button>
+     </div>
+    </div>
+  );
+}
+
+export default App;
